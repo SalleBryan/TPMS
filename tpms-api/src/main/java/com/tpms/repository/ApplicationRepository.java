67799e95b8ae362acaf1ba56cbabb9ef46cdbaab
Salle Bryan <nsalleyimnaiume@gmail.com>
2025-06-05 03:05:20 +0100

    Fixed Report Functionality

--- tpms-api/src/main/java/com/tpms/repository/ApplicationRepository.java
+++ tpms-api/src/main/java/com/tpms/repository/ApplicationRepository.java
@@ -14,7 +14,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
     @Query(
     value = "SELECT AVG(DATEDIFF(i.scheduled_at, a.applied_at)) " +
-            "FROM application a JOIN interview i " +
+            "FROM application a JOIN interviews i " +
             "ON a.student_id = i.student_id AND a.job_id = i.job_id",
     nativeQuery = true
        )
@@ -23,7 +23,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
        // value = "SELECT AVG(DATEDIFF('DAY', a.applied_at, i.scheduled_at)) " +
        //        "FROM application a JOIN interview i " +
        //        "ON a.student_id = i.student_id AND a.job_id = i.job_id",
-       // nativeQuery = trueE
+       // nativeQuery = true
        // )
        // Double findAverageTimeToPlacement();
 }
